eafd95a4f622936a9d0f0a2a221bf1c226c946ee
PhantomKidding <phantomkidding@gmail.com>
2016-09-09 16:13:11 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     from crawler import WeiboCrawler
     from login import WeiboLogin
 
-    username = raw_input('请输入微博账号 >')
-    pwd = raw_input('请输入微博密码 >')
-    keywords = raw_input('请输入查询关键词 >')
+    username = raw_input('Input Weibo account >') 
+    pwd = raw_input('Input password >')
+    keywords = raw_input('Input what you want to search >')
 
     WeiboLogin(username, pwd).login()
     WeiboCrawler(isConnectMySQL=True, htmlOutputDir='/Users/cchen224/Downloads/weibo').search(keywords)
